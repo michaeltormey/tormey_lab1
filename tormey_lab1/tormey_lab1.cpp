de2d5d22d90fd5d59ad56a7edd73970bbd37ded7
michaeltormey <chocolatethundu33@gmail.com>
2026-04-06 14:50:54 -0500

    This message edition.

--- tormey_lab1/tormey_lab1.cpp
+++ tormey_lab1/tormey_lab1.cpp
@@ -2,12 +2,14 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello from Michael Tormey. This is my first Git commit.\n";
-}
+    cout << "Hello from Michael Tormey. This is my first Git commit.\n";
 
+    cout << "Deez Nuts.\n";
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
