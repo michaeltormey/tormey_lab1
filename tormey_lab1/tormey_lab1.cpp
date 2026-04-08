d1d944861954859ffc93fe9dc09f264198a3f0aa
michaeltormey <chocolatethundu33@gmail.com>
2026-04-08 14:57:09 -0500

    They told me to

--- tormey_lab1/tormey_lab1.cpp
+++ tormey_lab1/tormey_lab1.cpp
@@ -11,6 +11,8 @@ int main()
     cout << "Deez Nuts.\n";
 
     cout << "GitHub collaboration easy!\n";
+
+    cout << "Feature by Michael Tormey: Branching is awesome!" << endl;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
